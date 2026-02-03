5f4aebed44f46fd27af1d740b4cde283da27beaf
SriVignesh-Space <srivignesh1149@gmail.com>
2026-02-03 23:05:44 +0530

    feat : logging filter added

---
+++ src/main/java/com/storemanagement/vinyl/Middleware/Logging.java
@@ -0,0 +1,24 @@
+package com.storemanagement.vinyl.Middleware;
+
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+public class Logging implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest Request, ServletResponse Response, FilterChain chain) throws IOException, ServletException{
+        HttpServletRequest request = (HttpServletRequest) Request;
+        System.out.println(request.getMethod() + " : " + request.getRequestURI() + "Incomming bro");
+        chain.doFilter(Request, Response);
+        System.out.println(request.getMethod() + " : " + request.getRequestURI() + "Response Sent");
+    }
+}
